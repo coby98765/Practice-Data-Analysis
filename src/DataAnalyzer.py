58e5b7e1ef240daeb3f3a5984957e2d87f049cd6
coby98765 <coby98765@gmail.com>
2025-08-04 15:33:14 +0300

    DataAnalyzer class method longest_tweets

--- src/DataAnalyzer.py
+++ src/DataAnalyzer.py
@@ -1,16 +1,13 @@
-from abc import abstractmethod
-
 import pandas as pd
 
 class DataAnalyzer:
     def __init__(self,df):
         self.df = df
-        self.df_antisemitic = self.split_df(self.df,1)
-        self.df_non_antisemitic = self.split_df(self.df,0)
+        self.df_antisemitic = self.split_df(1)
+        self.df_non_antisemitic = self.split_df(0)
 
-    @staticmethod
-    def split_df(df,val):
-        filtered = df.where(df["Biased"]==val)
+    def split_df(self,val):
+        filtered = self.df.where(self.df["Biased"]==val)
         clean_df = filtered.dropna()
         return clean_df
 
@@ -19,14 +16,22 @@ class DataAnalyzer:
         count_dict["total"] = len(self.df)
         return count_dict
 
-    def tweets_len(self):
-        pass
-
     def longest_tweets(self):
-        pass
+        len_dict = dict()
+        len_dict["antisemitic"] = DataAnalyzer._longest_text_in_df(self.df_antisemitic)
+        len_dict["non_antisemitic"] = DataAnalyzer._longest_text_in_df(self.df_non_antisemitic)
+        len_dict["total"] = DataAnalyzer._longest_text_in_df(self.df)
+        return len_dict
 
     def most_popular_words(self):
         pass
 
     def caps_word_count(self):
         pass
+
+    @staticmethod
+    def _longest_text_in_df(df):
+        sorted_df = df.sort_values(by='Text',
+                                   ascending=False,
+                                   key=lambda msg: msg.str.len())
+        return sorted_df[:3].to_dict(orient="index")
