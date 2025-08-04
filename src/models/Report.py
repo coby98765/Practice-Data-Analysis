47b924c979d01241eb6f4213dac4abf22ddeedb0
coby98765 <coby98765@gmail.com>
2025-08-04 11:33:55 +0300

    Report model class all methods done

--- src/models/Report.py
+++ src/models/Report.py
@@ -2,6 +2,7 @@ class Report:
     def __init__(self):
         self.total_tweets = dict()
         self.average_length = dict()
+        self.common_words = dict()
         self.longest_3_tweets = dict()
         self.uppercase_words = dict()
 
@@ -14,11 +15,39 @@ class Report:
         total_dict["<unspecified>"] = count - sub_total
         self.total_tweets = total_dict
 
+    def set_average_length(self, antisemitic,non_antisemitic,total):
+        average_len_dict = dict()
+        average_len_dict["antisemitic"] = antisemitic
+        average_len_dict["non_antisemitic"] = non_antisemitic
+        average_len_dict["total"] = total
+        self.average_length = average_len_dict
+
+    def set_common_words(self, antisemitic,non_antisemitic,total):
+        common_words_dict = dict()
+        common_words_dict["antisemitic"] = antisemitic
+        common_words_dict["non_antisemitic"] = non_antisemitic
+        common_words_dict["total"] = total
+        self.common_words = common_words_dict
+
+    def set_longest_tweets(self, antisemitic,non_antisemitic,total):
+        longest_tweets_dict = dict()
+        longest_tweets_dict["antisemitic"] = antisemitic
+        longest_tweets_dict["non_antisemitic"] = non_antisemitic
+        longest_tweets_dict["total"] = total
+        self.longest_3_tweets = longest_tweets_dict
+
+    def set_uppercase_words(self, antisemitic,non_antisemitic,total):
+        uppercase_dict = dict()
+        uppercase_dict["antisemitic"] = antisemitic
+        uppercase_dict["non_antisemitic"] = non_antisemitic
+        uppercase_dict["total"] = total
+        self.uppercase_words = uppercase_dict
 
     def to_dict(self):
         report = {
             "total_tweets" : self.total_tweets,
             "average_length": self.average_length,
+            "common_words": self.common_words,
             "longest_3_tweets": self.longest_3_tweets,
             "uppercase_words": self.uppercase_words,
         }
